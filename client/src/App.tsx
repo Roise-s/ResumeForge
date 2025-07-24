d38cc2a764844bfd3d7ca2b2d55ae14069798a1f
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-07-24 15:11:00 +0200

    update 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
+import Create from './pages/Create'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/" element={<Create />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/sections/Main.tsx
+++ client/src/sections/Main.tsx
@@ -5,9 +5,12 @@ const Main = () => {
     <div className='main-container'>
         <p className='mainTitle'>Built by a developer <br /> For developers</p>
         <p className='subIntro'>Free and open-source resume builder that helps you create professional resumes in minutes.</p>
-        <div className="innerbtn">
+        <a href="/create">
+          <div className="innerbtn">
             <button className='linkPass'>Create your resume <FaArrowRightFromBracket className="icon" /></button>
-        </div>
+          </div>
+        </a>
+        
     </div>
   )
 }
